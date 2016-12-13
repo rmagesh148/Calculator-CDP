55508ae64402790643bdf620569352059105dd62
Magesh Rajasekaran <Magesh Rajasekaran>
2016-12-13 13:40:55 -0600

    Application class added

--- calculator.py
+++ calculator.py
@@ -126,3 +126,128 @@ Desc: Invoker class invokes the commands(each expression) to execute
 
 	def store_and_execute(self, invoker_object):
 		invoker_object.execute()
+
+class Application(object):
+	"""
+	Input : Gets the input from user line by line
+	Return: Returns the evaluated total value
+	Desc: Application class communicates with the user to get the expression and returns the value
+	"""
+	user = CalculatorInvoker()
+	
+	input_commands = []
+
+	clear_list = False
+
+	while True:
+		calc = Calculator()
+
+		input_value = raw_input('Enter the expression one by one please: ')
+		
+		if input_value == 'Q':
+			break
+
+		if input_value == '=':
+
+			list_alpha_symbols = ['+', '-', 'A', 'C', '*', '/']
+			
+			if input_commands:
+			
+				if input_commands[0] not in list_alpha_symbols: 
+					add_object = Addition(calc, input_commands[0])
+					user.store_and_execute(add_object)
+					input_commands.pop(0)
+				else:
+					print "RaiseError: Type The Number First Always!"
+					print "Please enter from Scratch!!"
+					del input_commands[:]
+			else:
+				print "No commands entered!"
+			
+			for value in range(0, len(input_commands), 2):
+				if input_commands[value] == '+':
+					if value < len(input_commands):
+						add_object = Addition(calc, input_commands[value + 1])
+						user.store_and_execute(add_object)
+						
+				if input_commands[value] == '-':
+					if value < len(input_commands):
+						sub_object = Subtraction(calc, input_commands[value + 1])
+						user.store_and_execute(sub_object)
+						
+				if input_commands[value] == '*':
+					if value < len(input_commands):
+						mul_object = Multiplication(calc, input_commands[value + 1])
+						user.store_and_execute(mul_object)
+						
+				if input_commands[value] == '/':
+					if value < len(input_commands):
+						div_object = Division(calc, input_commands[value + 1])
+						user.store_and_execute(div_object)
+			
+			print "************************************************"
+			if (isinstance(calc.total, int)):
+				print "Total Value Evaluated to {0} ".format(calc.total)
+			else:		
+				print "Total Value Evaluated to {0:.15f} ".format(calc.total)
+			print "************************************************"
+			input_commands = []
+
+		else:
+			if clear_list:
+				clear_list = False
+			else:	
+				
+				list_symbols = ['+', '-', '*', '/']
+				
+				if input_value.lstrip('-').replace('.','').isdigit(): 
+					try:
+						if input_value.lstrip('-').isdigit():
+							value = int(input_value)
+							input_commands.append(value)
+						else:
+							try:
+								value = float(input_value)
+								input_commands.append(value)
+							except:
+								pass
+					except:
+						pass
+
+				elif input_value.isalpha():
+					if input_value == 'C':
+						input_commands.pop()
+						input_commands.pop()
+					elif input_value == 'A':
+						del input_commands[:]
+						clear_list = True
+					else:
+						print "Only, A - All Clear, Q- Quit & C- Clear the previous, are allowed"
+					
+				elif len(input_value.split(' ')) == 2:
+					split_values = input_value.split(' ')
+					if split_values[1] == '1/X' or split_values[1] == '1/x':
+						try:
+							split_number = float(split_values[0])
+							inv_object = Inverse(calc, split_number)
+							user.store_and_execute(inv_object)
+							input_commands.append(calc.inv_value)
+						except:
+							print "ValueError: I didn't get the coefficient; Please correct it"
+					else:
+						print "Do you mean 1/x? If so please type the coefficient<space>1/x"
+				
+				elif input_value[len(input_value)-1] == "!":
+					if input_value[:len(input_value)-1].isdigit():
+						fact_object = Factorial(calc, int(input_value[:len(input_value)-1]))
+						user.store_and_execute(fact_object)
+						input_commands.append(calc.fact_value)
+					else:
+						print "Factorial only accepts integral values"
+				
+				
+				
+				elif input_value in list_symbols:
+					input_commands.append(input_value)
+				else:
+					print "'+', '-', '*', '/' Only These Symbols Are Allowed and Numbers are allowed"
